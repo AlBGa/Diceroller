1f12cb54004f67894b31fb1fa111a3443743edb6
Alberto Barreto <albarretogal@gmail.com>
2018-05-26 21:29:10 +0200

    rebound effect on the dice

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* 
+* 1: rotate3d(0, 0, 0, 0deg)
+* 2: rotate3d(0, 1, 0, 90deg), rotate3d(1, 0, -1, 180deg), rotate3d(1, 1, -1, 120deg)
+* 3: rotate3d(0, 1, -1, 180deg), rotate3d(1, 0, 0, -90deg), rotate3d(1, 1, -1, -120deg)
+* 4: rotate3d(1, 0, 0, 90deg), rotate3d(0, 1, 1, 180deg), rotate3d(1, 1, 1, 120deg)
+* 5: rotate3d(0, 1, 0, -90deg), rotate3d(1, 0, 1, 180deg), rotate3d(1, 1, 1, -120deg) 
+* 6: rotate3d(0, 1, 0, 180deg), rotate3d(1, 0, 0, 180deg), rotate3d(1, 1, 0, 180deg)
+*/ 
+
+
