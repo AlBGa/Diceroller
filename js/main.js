d26cf031e11f4f56a180859c75ddb8b5acb397d5
Alberto Barreto <albarretogal@gmail.com>
2018-05-27 20:45:45 +0200

    button added

--- js/main.js
+++ js/main.js
@@ -7,4 +7,39 @@
 * 6: rotate3d(0, 1, 0, 180deg), rotate3d(1, 0, 0, 180deg), rotate3d(1, 1, 0, 180deg)
 */ 
 
+function clearclass(elem) {
+	elem.classList.remove("rollone", "rolltwo", "rollthree", "rollfour", "rollfive", "rollsix");
+}
 
+document.getElementById("roll").addEventListener("click", function(e) {
+	e.preventDefault();
+	var die = document.getElementById("maindie");
+	clearclass(die);
+	var rollednumber = Math.random() * 6 + 1;
+	console.log(parseInt(rollednumber));
+	
+	
+	switch(parseInt(rollednumber)) {
+		case 1:
+			die.classList.add("rollone");
+			break;
+		case 2:
+			die.classList.add("rolltwo");
+			break;
+		case 3:
+			die.classList.add("rollthree");
+			break;
+		case 4:
+			die.classList.add("rollfour");
+			break;
+		case 5:
+			die.classList.add("rollfive");
+			break;
+		case 6:
+			die.classList.add("rollsix");
+			break;
+		default:
+			console.log("This ain't supposed to happen.");
+			break;
+	}
+});
